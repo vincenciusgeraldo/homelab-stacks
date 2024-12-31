343dcb9552ad44cff4ab39e6a59c52e16e118d48
Vincencius Geraldo <vincenciusgeraldo@gmail.com>
2024-12-31 18:55:05 +0700

    Update Docker Compose Script

--- start-docker-compose.sh
+++ start-docker-compose.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-docker-compose -f infrastructure/docker-compose.yaml up -d
-docker-compose -f utilities/docker-compose.yaml up -d
-docker-compose -f media-management/docker-compose.yaml up -d
-docker-compose -f media-sharing/docker-compose.yaml up -d
+/usr/bin/docker compose -f infrastructure/docker-compose.yaml up -d
+/usr/bin/docker compose -f utilities/docker-compose.yaml up -d
+/usr/bin/docker compose -f media-management/docker-compose.yaml up -d
+/usr/bin/docker compose -f media-sharing/docker-compose.yaml up -d
